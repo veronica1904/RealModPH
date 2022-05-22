80d3bb13a76e6354ce8edcead946037ac8c4347a
unknown <jarlingduqueduarte@gmail.com>
2022-05-22 18:09:33 -0500

    testing

--- src/routes/App.js
+++ src/routes/App.js
@@ -34,7 +34,6 @@ const App = () => {
       <div className='container-loader'>
       <img width='100px' src='https://ventanillavirtual.micm.gob.do/Images/straight-loader.gif'/>
       <img width='200px' src='https://res.cloudinary.com/veronicaduque/image/upload/v1652965425/RealMod/Logo_ofgtb3.png'/>
-      
   </div>
     )
   }
